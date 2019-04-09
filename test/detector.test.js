15c2119b4ff423c50585994b49a4ccadc957c8a4
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-04-09 07:20:58 -0400

    Removing trailing whitespaces

--- test/detector.test.js
+++ test/detector.test.js
@@ -1,12 +1,12 @@
 // const request = require('request')
 // const expect = require('chai').expect
-// 
+//
 // let detector = require('../src/functions/detector')
-// 
+//
 // describe('detector', function () {
 //   it('should return nothing', async function () {
 //     let result = await detector.detector({})
-// 
+//
 //     expect(result).to.eql(undefined)
 //   })
 // })
--- test/executor.test.js
+++ test/executor.test.js
@@ -1,12 +1,12 @@
 // const request = require('request')
 // const expect = require('chai').expect
-// 
+//
 // let executor = require('../src/functions/executor')
-// 
+//
 // describe('executor', function () {
 //   it('should return nothing', async function () {
 //     let result = await executor.executor({})
-// 
+//
 //     expect(result).to.eql(undefined)
 //   })
 // })
