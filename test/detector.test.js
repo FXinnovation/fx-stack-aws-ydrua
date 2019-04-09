00e0ad778a303b42254153c7e6ec64a88eb82b77
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-04-09 10:02:57 -0400

    Added some tests

--- test/detector.test.js
+++ test/detector.test.js
@@ -1,24 +1,10 @@
-// const request = require('request')
-// const expect = require('chai').expect
-//
-// let detector = require('../src/functions/detector')
-//
-// describe('detector', function () {
-//   it('should return nothing', async function () {
-//     let result = await detector.detector({})
-//
-//     expect(result).to.eql(undefined)
-//   })
-// })
+const expect = require('chai').expect
+let detector = require('../src/functions/detector')
 
-// var AWS = require('aws-sdk-mock')
-//
-// AWS.mock('Cloudformation', 'listStacks', function (params, callback) {
-//   callback(null, 'test')
-// })
-//
-// describe('foo', function () {
-//
-// })
-//
-// AWS.restore('Cloudformation', 'listStacks')
+describe('detector', function () {
+  it('should return nothing', async function () {
+    let result = await detector.detector({})
+
+    expect(result).to.eql(undefined)
+  })
+})
